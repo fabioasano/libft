c6d4fd7d43d43dc32e0168bd02094264418e4767
Fabio Casa <fabioasa@gmail.com>
2022-09-20 21:02:03 -0300

    add ft_strdup.c

---
+++ ft_strdup.c
@@ -0,0 +1,17 @@
+char	*ft_strdup(const char *src)
+{
+	char	*ptr;
+	size_t	i;
+
+	i = 0;
+	ptr = (char *)malloc(sizeof(char) * ft_strlen(src + 1));
+	if (ptr == NULL)
+		return (NULL);
+	while (src[i] != '\0')
+	{
+		ptr[i] = src[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
