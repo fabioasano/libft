fc1d022bf6766a257458191d3ade717723208bcf
Fabio Casa <fabioasa@gmail.com>
2022-09-28 10:00:12 -0300

    ft_memcpy.c adjust

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *scr, size_t n)
 {
